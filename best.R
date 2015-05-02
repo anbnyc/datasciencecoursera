82d14d7fd5936a53f6cbd1a2d0870243f233d1cf
anbnyc <alecbarrett@gmail.com>
2015-05-02 18:32:18 -0400

    update best.R

--- best.R
+++ best.R
@@ -1,24 +1,38 @@
-setwd("J:/R")
-outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-head(outcome)
+# setwd("~/Desktop/R")
+# outcome <- read.csv("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character")
 
-outcome[,11] <- as.numeric(outcome[,11])
-hist(outcome[,11])
+# head(outcome)
+# outcome[,11] <- as.numeric(outcome[,11])
+# hist(outcome[,11])
 
-best <- function(state, outcome){
-    colNum <- if (outcome = "heart attack") {
-        11
-    } else if (outcome = "heart failure") {
-        17
-    } else if (outcome = "pneumonia") {
-        23
-    } else {
-        NA
+best <- function(state, outcome) {    
+    
+    data <- .GlobalEnv$outcome
+    
+    if (!(state %in% data$State)) {
+        stop("invalid state")
     }
     
-    outcome[,colNum] <- as.numeric(outcome[,colNum])
+    colNum <- if (outcome == "heart attack") {
+        11
+    } else if (outcome == "heart failure") {
+        17
+    } else if (outcome == "pneumonia") {
+        23
+    } else {
+        stop("invalid outcome")
+    }
     
-    mins <- tapply(outcome[,colNum], outcome$State, min, na.rm = TRUE, simplify=FALSE) ## mins is an array
-    mins <- as.matrix(mins) ## mins is a matrix
-    statemin <- as.numeric(mins[state,])
+    data <- data[order(data$Hospital.Name),]
+    data <- data[which((data[,colNum])!="Not Available"),]
+    data[,colNum] <- as.numeric(data[,colNum])
+    
+    mins <- tapply(data[,colNum], data$State, min, na.rm = TRUE, simplify=FALSE) ## mins is an array
+    mins <- as.matrix(mins)                                                      ## mins is a matrix
+    statemin <- mins[state,]
+
+    hospitals <- data[which (data[,colNum] == statemin & data$State == state),]
+ 
+    thebest <- hospitals[1, "Hospital.Name"]
+    thebest
 }
